5cce501962ddd8a42c1e7d0361acf0873537c1b0
Mohammad Ammar <theammarngp@gmail.com>
2026-06-16 23:31:23 +0530

    Alias.sql

---
+++ 01_Fundamentals/03_ALIAS.sql
@@ -0,0 +1,14 @@
+-- Q1
+-- Rename emp_name column
+
+SELECT
+    emp_name AS Employee_Name
+FROM sqlemployees;
+
+
+-- Q2
+-- Rename count column
+
+SELECT
+    COUNT(*) AS Total_Employees
+FROM sqlemployees;
