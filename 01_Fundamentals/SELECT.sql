b50084e7c3b06e981495f854a3385cb6322e3ccf
Mohammad Ammar <theammarngp@gmail.com>
2026-06-16 23:21:16 +0530

    SELECT.sql
    Added SQL queries to retrieve employee information.

---
+++ 01_Fundamentals/SELECT.sql
@@ -0,0 +1,21 @@
+-- TOPIC: SELECT
+
+-- Q1
+-- Show all employee names
+
+SELECT emp_name
+FROM sqlemployees;
+
+-- Q2
+-- Show all employee ids
+
+SELECT emp_id
+FROM sqlemployees;
+
+-- Q3
+-- Show employee name and department id
+
+SELECT
+    emp_name,
+    dept_id
+FROM sqlemployees;
